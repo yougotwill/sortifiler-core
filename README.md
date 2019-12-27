f88301920470fd95a546de441237c990eb14af17
William Grant <willmgrant@gmail.com>
2019-12-27 13:35:08 +1100

    ready to publish

---
+++ README.md
@@ -0,0 +1,124 @@
+# SortiFiler
+
+> Get your files sorted. 📚🗂
+
+## Description
+An opinionated npm package to sort your files and folders easily.
+
+**CLI & Desktop app coming soon! 🚀**
+
+Everyday you look at your *Desktop/Downloads* folder and think, "Damn I need to clean up this mess". 🤦
+
+That feeling is why this package now exists. 😆
+
+SortiFiler classifies files and folders and moves them into *\_Type* folders for easy access at the root of your chosen directory. 🗃
+
+## Installation
+
+To add SortiFiler to your project use `npm install sortifiler`.
+
+If you are looking for the SortiFiler CLI (Terminal Program) then check out [sortifiler-cli](https://github.com/yougotwill/sortifiler-cli)!
+
+## Usage
+
+```js
+const sortifiler = require('sortifiler');
+
+sortifiler.sortAll('~/Desktop');
+// sorts your Desktop into _Type folders
+```
+
+## API
+
+### sortAll(folderPath, metaFiles?)
+
+Sorts all files and folders in a given directory.
+
+#### folderPath
+
+Type: `string`
+
+The path to the directory you want to sort.
+
+#### metaFiles
+
+Type: `boolean`
+Default: `false`
+
+Sort *.meta* files as well.
+
+### sortFiles(folderPath, metaFiles?)
+
+Sorts all files in a given directory.
+
+#### folderPath
+
+Type: `string`
+
+The path to the directory you want to sort.
+
+#### metaFiles
+
+Type: `boolean`
+Default: `false`
+
+Sort *.meta* files as well.
+
+### sortFolders(folderPath, metaFiles?)
+
+Sorts all folders in a given directory.
+
+#### folderPath
+
+Type: `string`
+
+The path to the directory you want to sort.
+
+#### metaFiles
+
+Type: `boolean`
+Default: `false`
+
+Sort *.meta* files as well.
+
+## How does the sorting work?
+
+- Folders are classified using the best matching *_Type* folder based on the files within that folder (only 1 level down).
+- Files are classified as follows:
+
+| _Type Folder | File Extension                           |
+| :------------- | :--------------------------------------- |
+| _Books         | ".epub", ".mobi"                         |
+| _Documents     | ".pdf", ".txt", ".doc", ".docx", ".ppt", ".pptx", ".md", ".json", ".ods", ".log", ".xls", ".xlsx", ".ttf" |
+| _Images        | ".png", ".jpg", ".jpeg", ".gif", ".xcf", ".stl", ".blend", "*.obj", "*.mtl", "*.3ds", "*.tga", ".icns" |
+| _Music         | ".mp3", ".wav", ".flac", ".m4a", ".ogg", ".mid", ".asd", ".m3u", ".pls", ".alp", ".asx", ".bfxrsound", ".m3u8", ".als", ".m4r" |
+| _Programs      | ".dmg", ".exe", ".sh", ".app", ".pkg", ".apk", ".ipa", ".gba", ".gbc" |
+| _Scripts       | ".py", ".java", ".class", ".sh", "*.cs", "*.r", ".itermcolors", ".terminal", ".theme", ".gbaskin", ".tmtheme", ".resbackup" |
+| _Torrents      | ".torrent"                               |
+| _Videos        | ".mkv", ".mp4", ".mov", ".mpeg", ".webm", ".srt", ".avi" |
+| _Web           | ".html", ".css", ".js", ".htm"           |
+| _Zipped        | ".zip", ".rar", ".7z", ".tar.gz", ".tar", ".gz", "*.unitypackage", "*.prefab", ".fbx" |
+
+- **Note:** This list maybe outdated please check the [SORTLIST](index.js) for the latest info.
+
+## Development
+
+### Contributing
+
+1. Fork it
+2. Create your feature branch: `git checkout -b feature/my-new-feature`
+3. Commit your changes: `git commit -am 'Add some feature'`
+4. Push to the branch: `git push origin feature/my-new-feature`
+5. Submit a pull request
+
+### Requirements / Dependencies
+
+- npm
+
+## Version
+
+0.1.0
+
+## License
+
+[MIT](LICENSE)
---
+++ index.js
@@ -0,0 +1,107 @@
+'use strict';
+
+const fsJetpack = require('fs-jetpack');
+const parseTorrent = require('parse-torrent');
+
+const SORTLIST = { // order is based on sorting priority
+  '_Torrents': ['*.torrent'],
+  '_Images': ['*.png', '*.jpg', '*.jpeg', '*.gif', '*.xcf', '*.stl', '*.blend', '*.obj', '*.mtl', '*.3ds', '*.tga', '*.icns', '*.piskel', '*.heif', '*.svg'],
+  '_Music': ['*.mp3', '*.wav', '*.flac', '*.m4a', '*.ogg', '*.mid', '*.asd', '*.m3u', '*.pls', '*.alp', '*.asx', '*.bfxrsound', '*.m3u8', '*.als', '*.m4r', '*.opus'],
+  '_Videos': ['*.mkv', '*.mp4', '*.mov', '*.mpeg', '*.webm', '*.srt', '*.avi'],
+  '_Books': ['*.epub', '*.mobi'],
+  '_Scripts': ['*.py', '*.java', '*.class', '*.sh', '*.cs', '*.r', '*.itermcolors', '*.terminal', '*.theme', '*.gbaskin', '*.deltaskin', '*.tmtheme', '*.resbackup', '*.xccolortheme', '*.js', '*.cottheme'],
+  '_Web': ['*.html', '*.css', '*.js', '*.htm'],
+  '_Programs': ['*.dmg', '*.exe', '*.sh', '*.app', '*.pkg', '*.apk', '*.ipa', '*.gba', '*.gbc', '*.iso', '*.jar', '*.z64'],
+  '_Zipped': ['*.zip', '*.rar', '*.7z', '*.tar.gz', '*.tar', '*.gz', '*.unitypackage', '*.prefab', '*.fbx'],
+  '_Documents': ['*.pdf', '*.txt', '*.doc', '*.docx', '*.dotx', '*.ppt', '*.pptx', '*.md', '*.json', '*.ods', '*.log', '*.xls', '*.xlsx', '*.ttf']
+};
+
+const FILE_TYPES = Object.keys(SORTLIST);
+
+const _move = (object, newFolder, rootFolder, metaFiles = false) => {
+  rootFolder.dir(`${newFolder}`);
+  rootFolder.move(`${rootFolder.cwd()}/${object}`, `${rootFolder.cwd()}/${newFolder}/${object}`);
+  if (metaFiles && rootFolder.exists(`${object}.meta`) !== false) {
+    rootFolder.move(`${rootFolder.cwd()}/${object}.meta`, `${rootFolder.cwd()}/${newFolder}/${object}.meta`);
+  }
+};
+
+const _matchFiles = (extensions, rootFolder) => {
+  return rootFolder.find('.', {
+    matching: extensions,
+    recursive: false,
+    ignoreCase: true
+  });
+};
+
+const sortFiles = (folderPath, metaFiles = false) => {
+  const rootFolder = fsJetpack.cwd(folderPath);
+  const files = rootFolder.list();
+  if (!files) { return; }
+
+  FILE_TYPES.forEach((fileType) => {
+    const foundFiles = _matchFiles(SORTLIST[fileType], rootFolder);
+    if (foundFiles.length > 0) {
+      foundFiles.forEach((file) => {
+        if (rootFolder.exists(file) !== 'file') { return; }
+        if (fileType === '_Torrents') {
+          const torrent = rootFolder.read(`${rootFolder.cwd()}/${file}`, 'buffer');
+          try {
+            let torrentData = parseTorrent(torrent);
+            torrentData = torrentData.name;
+            if (rootFolder.exists(torrentData)) {
+              _move(`${torrentData}`, fileType, rootFolder);
+            }
+          } catch (e) {
+            console.error(e);
+          }
+        }
+
+        if (fileType === '_Web') {
+          const nameIndex = file.indexOf('.htm'); // includes .html
+          if (nameIndex === -1) {
+            return;
+          }
+          const fileData = `${file.substr(0, nameIndex)}_files`;
+          if (rootFolder.exists(fileData)) {
+            _move(`${fileData}`, fileType, rootFolder);
+          }
+        }
+
+        _move(`${file}`, fileType, rootFolder, metaFiles);
+      });
+    }
+  });
+};
+
+const sortFolders = (folderPath, metaFiles = false) => {
+  const rootFolder = fsJetpack.cwd(folderPath);
+  const folders = rootFolder.list();
+  if (!folders) { return; }
+
+  folders.forEach((folder) => {
+    if (FILE_TYPES.indexOf(folder) !== -1) { return; }
+    if (rootFolder.exists(folder) !== 'dir') { return; }
+
+    const childFolder = rootFolder.cwd(`${rootFolder.cwd()}/${folder}`);
+    let bestFileCount = 0;
+    let bestMatch = '';
+    FILE_TYPES.forEach((fileType) => {
+      const fileCount = _matchFiles(SORTLIST[fileType], childFolder).length;
+      if (fileCount > bestFileCount) {
+        bestFileCount = fileCount;
+        bestMatch = fileType;
+      }
+    });
+
+    if (bestMatch === '') { return; }
+    _move(folder, bestMatch, rootFolder, metaFiles);
+  });
+};
+
+const sortAll = (folderPath, metaFiles = false) => {
+  sortFiles(folderPath, metaFiles);
+  sortFolders(folderPath, metaFiles);
+};
+
+module.exports = { sortFiles, sortFolders, sortAll };
